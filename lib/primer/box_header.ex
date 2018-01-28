354c151bf060e44d433224a411b1da5deb4e60a4
Lee Dohm <1038121+lee-dohm@users.noreply.github.com>
2018-01-28 14:19:47 -0800

    Add blue modifier to BoxHeader

--- lib/primer/box_header.ex
+++ lib/primer/box_header.ex
@@ -2,5 +2,5 @@ defmodule Primer.BoxHeader do
   @moduledoc """
   Represents a [Box header element](https://github.com/primer/primer/tree/master/modules/primer-box).
   """
-  use Primer.Element
+  use Primer.Element, modifiers: [:blue]
 end
--- test/primer/box_header_test.exs
+++ test/primer/box_header_test.exs
@@ -1,3 +1,7 @@
 defmodule Primer.BoxHeaderTest do
   use Primer.ElementCase, module: Primer.BoxHeader
+
+  describe "modifiers" do
+    modifier_test Primer.BoxHeader, :blue
+  end
 end
