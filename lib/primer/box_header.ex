dded9ead3368523ace1cb2278b80465e34b5c034
Lee Dohm <1038121+lee-dohm@users.noreply.github.com>
2018-01-28 09:48:29 -0800

    Add the Box header element

---
+++ lib/primer/box_header.ex
@@ -0,0 +1,8 @@
+defmodule Primer.BoxHeader do
+  @moduledoc """
+  Represents a [Box header element](https://github.com/primer/primer/tree/master/modules/primer-box).
+  """
+  use Primer.Element
+
+  def class, do: "Box-header"
+end
